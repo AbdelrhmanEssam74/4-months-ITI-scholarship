083cebc013f2f5bba4aac3612dc0d20820b341e0
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-03-17 14:39:39 +0200

    week 2 / lab 5 point 5

--- week 2/Lab 5/Point 5/main.js
+++ week 2/Lab 5/Point 5/main.js
@@ -23,33 +23,33 @@
 
 //? Point b
 
-let API = "https://reqres.in/api/users"
-let ul = document.getElementById("dropdown-menu");
-
-let xhr = new XMLHttpRequest();
-xhr.open("GET", API, true);
-let data = {}
-xhr.onreadystatechange = () =>{
-    if (xhr.readyState === 4 && xhr.status === 200){
-        data = JSON.parse((xhr.responseText)).data
-        for (let i = 0; i <data.length ; i++) {
-            let li = document.createElement("li");
-            li.innerHTML = `<a href="#" onclick="displayUserData(${data[i].id})">${data[i].first_name} ${data[i].last_name}</a>`;
-            ul.appendChild(li);
-        }
-    }
-}
-function displayUserData(id){
-    API = `https://reqres.in/api/users/${id}`;
-    xhr.open("GET", API, true);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            data = JSON.parse(xhr.responseText).data;
-            document.getElementById("fname").textContent = data.first_name + " " + data.last_name;
-            document.getElementById("avatar").setAttribute("src", data.avatar)
-        }
-    }
-    xhr.send()
-}
-
-xhr.send()
+// let API = "https://reqres.in/api/users"
+// let ul = document.getElementById("dropdown-menu");
+//
+// let xhr = new XMLHttpRequest();
+// xhr.open("GET", API, true);
+// let data = {}
+// xhr.onreadystatechange = () =>{
+//     if (xhr.readyState === 4 && xhr.status === 200){
+//         data = JSON.parse((xhr.responseText)).data
+//         for (let i = 0; i <data.length ; i++) {
+//             let li = document.createElement("li");
+//             li.innerHTML = `<a href="#" onclick="displayUserData(${data[i].id})">${data[i].first_name} ${data[i].last_name}</a>`;
+//             ul.appendChild(li);
+//         }
+//     }
+// }
+// function displayUserData(id){
+//     API = `https://reqres.in/api/users/${id}`;
+//     xhr.open("GET", API, true);
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             data = JSON.parse(xhr.responseText).data;
+//             document.getElementById("fname").textContent = data.first_name + " " + data.last_name;
+//             document.getElementById("avatar").setAttribute("src", data.avatar)
+//         }
+//     }
+//     xhr.send()
+// }
+//
+// xhr.send()
