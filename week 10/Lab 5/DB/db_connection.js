a899fbb2596c874aa709d14ac27fcecdf983a97c
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-09 18:33:51 +0300

    Update database connection password
    The database connection password has been updated to enhance security. Ensure all environments are updated with the new credentials to maintain connectivity.

--- week 10/Lab 5/DB/db_connection.js
+++ week 10/Lab 5/DB/db_connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'admin',
-    password: '9((8ZlVaT176b1zc',
+    password: 'bVIO.d1DOr6YLWsH',
     database: 'express_api',
 })
 
