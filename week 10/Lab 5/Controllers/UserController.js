9487167390393c82b00ddad104dddf710dba96be
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-09 18:33:13 +0300

    Switch to database storage for user messages in contact form
    Replaced in-memory storage with a database insertion for received contact form messages. Added `storeUserMessage` function to handle database operations with proper error handling. Updated related logic to ensure smooth transition.

--- week 10/Lab 5/Controllers/UserController.js
+++ week 10/Lab 5/Controllers/UserController.js
@@ -1,6 +1,6 @@
 const users = require('../data/users-messages')
 const {validationResult} = require('express-validator');
-
+const db = require('../DB/db_connection')
 
 exports.contactUs = (req, res) => {
     res.render('contact', {
@@ -16,7 +16,7 @@ exports.validate = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.render('contact', {
-            errors : errors.array(),
+            errors: errors.array(),
             username: req.body.username,
             email: req.body.email,
             message: req.body.message,
@@ -28,7 +28,8 @@ exports.validate = (req, res) => {
             email: req.body.email,
             message: req.body.message
         }
-        users.push(userMessage);
+        // Store the user message in the database
+        storeUserMessage(userMessage);
         res.render('contact', {
             success: 'Message sent successfully',
             errors: [],
@@ -39,3 +40,14 @@ exports.validate = (req, res) => {
     }
 }
 
+function storeUserMessage(userMessageObj) {
+    const sql = 'INSERT INTO `ContactMessages`  (`username`, `email`, `message`) VALUES (?, ?, ?)';
+    const values = [userMessageObj.username, userMessageObj.email, userMessageObj.message];
+
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            return 0;
+        }
+        return 1;
+    });
+}
