d38633e52b3a83896a76aa6d5cf0ed07b308a392
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-12 02:24:52 +0300

    Add user list template and styling to user card component
    Implement a dynamic user list display with conditional rendering for empty states. Enhance the component's appearance with grid-based responsive design and role-specific badge styles. Improve usability with hover effects and optimized layouts for various screen sizes.

--- week 11/Lab2/Users-Project/src/app/user-card/user-card.component.ts
+++ week 11/Lab2/Users-Project/src/app/user-card/user-card.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-user-card',
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.css']
