c591ba4506af2e0e2ae18ccdef0e8ac7bee5e4e5
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-12 01:04:32 +0300

    Lab 2

---
+++ week 11/Lab2/Users-Project/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import userData from '../data/users.json';
+import { UserCardComponent } from './user-card/user-card.component';
+import { FormComponent } from './form/form.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [UserCardComponent, FormComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Users-Project';
+  users: any[] = userData;
+
+  ngOnInit() {
+    console.log('Loaded users:', this.users);
+  }
+}
---
+++ week 11/Lab2/Users-Project/src/app/form/form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {SearchIcon} from 'primeng/icons';
+@Component({
+  selector: 'app-form',
+  imports: [
+    SearchIcon
+  ],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+
+}
