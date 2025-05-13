2bd8c5ecc99689f161ea6024066ada8e66c981ac
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-13 15:37:40 +0300

    Enhance todo-wrapper layout and integrate todo form and list components

--- week 11/Lab3/Todo/src/app/components/todo-wrapper/todo-wrapper.component.ts
+++ week 11/Lab3/Todo/src/app/components/todo-wrapper/todo-wrapper.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {TodoListComponent} from '../todo-list/todo-list.component';
+import {TodoFormComponent} from '../todo-form/todo-form.component';
 
 @Component({
   selector: 'app-todo-wrapper',
-  imports: [],
+  imports: [
+    TodoListComponent,
+    TodoFormComponent
+  ],
   templateUrl: './todo-wrapper.component.html',
   styleUrl: './todo-wrapper.component.css'
 })
