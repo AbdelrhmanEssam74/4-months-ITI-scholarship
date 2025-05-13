c3bcb32372cb9f3a7c4836482a878036ad6439aa
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-13 15:38:13 +0300

    Replace placeholder content with TodoWrapper component and add styles for todo completion and actions

--- week 11/Lab3/Todo/src/app/app.component.ts
+++ week 11/Lab3/Todo/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {TodoWrapperComponent} from './components/todo-wrapper/todo-wrapper.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoWrapperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
