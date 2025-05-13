ccf2be35ddb3f0a3f34a345005fb4b1d241fc3ac
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-13 16:15:22 +0300

    Refactor todo-list component to accept dynamic task input and bind buttons with task IDs

--- week 11/Lab3/Todo/src/app/components/todo-list/todo-list.component.ts
+++ week 11/Lab3/Todo/src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-
+  @Input() taskItem: any;
 }
