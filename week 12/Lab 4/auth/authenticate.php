7dd3b0c55a0aebf1d87b76126e8bdc2d97baaee3
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-26 02:46:07 +0300

    Add user authentication and logout functionality
    Implemented authentication logic to handle login with email and password validation, including error handling and persistent login with cookies. Added logout functionality to clear user session and cookies. These features ensure secure access and proper session management.

---
+++ week 12/Lab 4/auth/authenticate.php
@@ -0,0 +1,42 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    session_start();
+    require_once '../db/db_connection.php';
+
+    $_SESSION['errors'] = [];
+
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if (empty($email) || empty($password)) {
+        $_SESSION['errors'][] = "Email and password are required.";
+    } else {
+        $stmt = $conn->prepare("SELECT * FROM users WHERE email = '$email'");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (count($result) === 0) {
+            $_SESSION['errors'][] = "No user found with that email.";
+        } else {
+            $user = $result[0];
+            if (!($password === $user['password'])) {
+                $_SESSION['errors'][] = "Incorrect password.";
+            }
+        }
+    }
+
+    if (!empty($_SESSION['errors'])) {
+        header("Location: ../index.php");
+        exit;
+    }
+
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['user_email'] = $user['email'];
+
+    if (isset($_POST['remember'])) {
+        setcookie('user_id', $user['id'], time() + (86400 * 100), "/");
+        setcookie('login', true, time() + (86400 * 100), "/");
+    }
+
+    header("Location: ../dashboard.php");
+    exit;
+}
