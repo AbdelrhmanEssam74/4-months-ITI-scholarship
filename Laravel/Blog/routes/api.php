a2a7209e649503cec45e758a21c80996f74d8edb
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-24 03:03:00 +0300

    Secure API routes with Sanctum middleware; add authentication routes for register, login, and logout.

--- Laravel/Blog/routes/api.php
+++ Laravel/Blog/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ArticleController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CommentsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,13 +12,25 @@ use Illuminate\Support\Facades\Route;
 //})->middleware('auth:sanctum');
 
 //Route::apiResource('articles',ArticleController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('articles', [ArticleController::class, 'store'])
+        ->name('articles.store');
+    Route::put('articles/{article:slug}', [ArticleController::class, 'update'])->name('articles.update');
+
+    Route::post('comments', [CommentsController::class, 'store'])
+        ->name('comments.store');
+    Route::put('comments/{id}', [CommentsController::class, 'update'])
+        ->name('comments.update');
+
+    Route::post('logout', [AuthController::class, 'logout'])
+        ->name('logout');
+});
+
 Route::get('articles', [ArticleController::class, 'index']);
-Route::post('articles', [ArticleController::class, 'store'])
-    ->name('articles.store');
-Route::put('articles/{article:slug}', [ArticleController::class, 'update']);
 
+Route::post('register', [AuthController::class, 'register'])
+    ->name('register');
+Route::post('login', [AuthController::class, 'login'])
+    ->name('login');
 
-
-Route::post('comments', [CommentsController::class, 'store'])
-    ->name('comments.store');
-Route::put('comments/{id}', [CommentsController::class, 'update'])->name('comments.update');
